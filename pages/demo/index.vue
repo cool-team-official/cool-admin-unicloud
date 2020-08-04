8d0be75d4df4a62c917f168cb3e60c977ee56d4f
icssoa <2570063477@qq.com>
2020-08-05 00:24:23 +0800

    优化组件预览

--- pages/demo/index.vue
+++ pages/demo/index.vue
@@ -1,12 +1,20 @@
 <template>
     <cl-layout>
         <div class="page-demo">
-            <div class="page-demo__list">
-                <component
-                    v-for="(item, index) in list"
-                    :is="item"
-                    :key="index"
-                ></component>
+            <div
+                class="block"
+                :ref="`col-${index + 1}`"
+                v-for="(item, index) in layout"
+                :key="index"
+            >
+                <transition-group name="fade">
+                    <component
+                        :ref="item2"
+                        :is="item2"
+                        :key="item2 + index2"
+                        v-for="(item2, index2) in item"
+                    ></component>
+                </transition-group>
             </div>
         </div>
     </cl-layout>
@@ -17,7 +25,7 @@ import BClUpload from "./components/b-cl-upload";
 import BVCopy from "./components/b-v-copy";
 import BClCrud from "./components/b-cl-crud";
 import BClForm from "./components/b-cl-form";
-import BClContextMenu from "./components/b-cl-context-menu.js";
+import BClContextMenu from "./components/b-cl-context-menu";
 import BErrorPage from "./components/b-error-page";
 
 export default {
@@ -39,20 +47,78 @@ export default {
 				"b-cl-form",
 				"b-cl-context-menu",
 				"b-error-page"
-			]
+			],
+			layout: [[], [], [], []]
 		};
+	},
+
+	mounted() {
+		this.append();
+	},
+
+	methods: {
+		getHeight(name) {
+			return this.$refs[name][0].offsetHeight;
+		},
+
+		selectCol() {
+			const h1 = this.getHeight("col-1");
+			const h2 = this.getHeight("col-2");
+			const h3 = this.getHeight("col-3");
+			const h4 = this.getHeight("col-4");
+
+			switch (Math.min(h1, h2, h3, h4)) {
+				case h1:
+					return 0;
+				case h2:
+					return 1;
+				case h3:
+					return 2;
+				case h4:
+					return 3;
+				default:
+					return 0;
+			}
+		},
+
+		append(index = 0) {
+			const i = this.selectCol();
+			const item = this.list[index];
+
+			if (!item) {
+				return false;
+			}
+
+			this.layout[i].push(item);
+
+			this.$nextTick(() => {
+				setTimeout(() => {
+					this.append(index + 1);
+				}, 100);
+			});
+		}
 	}
 };
 </script>
 
 <style lang="scss" scoped>
-.page-demo {
-	height: 100%;
-	width: 100%;
-	overflow: auto;
+.fade-enter-active,
+.fade-leave-active {
+	transition: all 0.5s ease;
+}
 
-	&__list {
-		column-count: 4;
+.fade-enter,
+.fade-leave-active {
+	opacity: 0;
+}
+
+.page-demo {
+	overflow-y: auto;
+
+	.block {
+		width: calc(25% - 10px);
+		margin-right: 10px;
+		float: left;
 	}
 
 	/deep/.scope {
@@ -122,12 +188,6 @@ export default {
 				color: #999;
 			}
 		}
-	}
-}
-
-@media screen and (max-width: 992px) {
-	.page-demo__list {
-		column-count: 2;
 	}
 }
 </style>
